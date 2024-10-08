570b9340972f5178eaf52f68dbc50f2fd4247e54
ndiawar <dndiawar20@gmail.com>
2024-10-08 20:36:30 +0000

    Mettre à jour les bugs fixées

--- app/views/layout.php
+++ app/views/layout.php
@@ -35,7 +35,7 @@ $currentAction = $_GET['action'] ?? 'Dashboard';
         <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 sidebar">
             <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
                 <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                    <img src="../public/images/logo.png" alt="Logo École de la Réussite" class="img-fluid" style="max-width: 60%;">
+                    <img src="../public/images/logo.png" alt="Logo École de la Réussite" class="img-fluid" style="max-width: 120%;">
                 </a>
                 <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
                     <li class="nav-item">
